19019a8fc1545a8e3d82a05c1c8f99ed5219762a
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-10-05 00:55:16 +0530

    api controller

--- controller/apiController.js
+++ controller/apiController.js
@@ -1,16 +1,15 @@
 import asyncHandler from 'express-async-handler';
 import multer from 'multer';
-import path from 'path';
+import crypto from 'crypto';
+import { uploadFile } from '../aws/s3.js'
+
 
 const home = asyncHandler(async (req, res) => {
     res.render('index')
 })
 
-const storage = multer.diskStorage({
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
+
+const storage = multer.memoryStorage();
 
 const extensionCheck = (req, file, cb) => {
     if (file.mimetype == "application/pdf") {
@@ -21,6 +20,10 @@ const extensionCheck = (req, file, cb) => {
         });
     }
 };
+
+
+const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
+
 
 const jobForm =  asyncHandler(async (req, res) => {
 
@@ -35,20 +38,35 @@ const jobForm =  asyncHandler(async (req, res) => {
     upload(req, res, asyncHandler(async (err) => {
         
         if (err?.code === 'FILE_EXTENSION') {
+
             res.send({
                 code: 400,
                 message: 'Only PDF file are allowed!'
-            })
+            });
+
         } else if (err?.code === 'LIMIT_FILE_SIZE'){
+
             res.send({
                 code: 400,
                 message: 'File size must be less then 2MB'
-            })
+            });
+
         }else{
-            res.send({
-                code: 200,
-                message: 'Resume Uploaded!'
-            })
+
+            const buffer = req.file.buffer;
+            const fileName = randomImageName();
+            const mimetype = req.file.mimetype;
+
+            // upload file in s3
+            const result = await uploadFile(buffer, fileName, mimetype)
+
+            
+            if(result.$metadata.httpStatusCode === 200){
+                res.send({
+                    code: 200,
+                    message: 'Resume Uploaded!'
+                })
+            }
         }
         
     
