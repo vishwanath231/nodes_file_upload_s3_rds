c81425bf92c37f8c302774600bf2c7608075c9a0
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-10-06 17:56:38 +0530

    api router

--- router/router.js
+++ router/router.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { home, jobForm } from '../controller/apiController.js';
+import { home, jobForm, getMysqlData } from '../controller/apiController.js';
 
 
 router.route('/form').post(jobForm);
 router.route('/').get(home);
-
+router.route('/all').get(getMysqlData);
 
 export default router;
